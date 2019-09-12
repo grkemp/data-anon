72272f1e359650db4175698041455608ddad40fe
grkemp <54956148+grkemp@users.noreply.github.com>
2019-09-11 17:04:59 -0700

    initial setup for data_anon.py

---
+++ data_anon.py
@@ -0,0 +1,17 @@
+ #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+    File name: data_anon.py
+    Author: Grant Rodgers Kemp
+    Date created: 09112019
+    Date last modified: 09112019
+    Python version: 3.6.9
+'''
+
+import pandas as pd
+import numpy as np
+
+def read_data():
+
+    return
